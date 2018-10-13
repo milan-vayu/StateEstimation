4e2419ea37ed2b13230853d3af8e94b86dde0831
rammilan007 <send2milankumar@gmail.com>
2018-10-13 13:33:12 +0530

    Implementation of EKF done

--- maincode.m
+++ maincode.m
@@ -31,9 +31,9 @@ gama=(phi-eye(5))*inv(A)*B;
 C=[k7 0 k8 0 0;0 k7 0 k8 0];
 %x(k+1)=phi*x(k)+gama*u+w(k)
 %% measurement generation
+vk=mvnrnd(mv,R,N);
 for i=1:N
-    vk=mvnrnd(mv,R);
-    y(:,i)=C*(x(:,i)-xss') + vk' ;
+    y(:,i)=C*(x(:,i)-xss') + vk(i,:)' ; %measurement in delta y form
 end
 
 %% implementation of KF
@@ -56,7 +56,38 @@ subplot(324),plot(T,x(4,:),T,xkk(4,:)), ylabel('x_4 (flux)'), title('State Varia
 subplot(325),plot(T,x(5,:),T,xkk(5,:)), ylabel('x_5 (angular velocity)'), title('State Variables'),legend('true', 'estimated')
 
 %% Implementation of EKF
+% measurement generation for EKF
+for i=1:N
+    ey(:,i)=C*x(:,i) + vk(i,:)' ; %measurement in delta y form
+end
+exkk(:,1)=0.9*x(:,1); %0.9*initial state 
+ePkk(:,:,1)=eye(5);
 
+for i=1:N
+    % Succesive Linearization
+    A=jacob(@(x)imdyn(1,x,u),exkk(:,i));
+    Bd=eye(5) ; %since noise was individually added 
+    phi=expm(A*Ts);
+    gamad=eye(5); % (phi-eye(5))*inv(A)*Bd;
+    % Prediction step
+    exkk1(:,i)=exkk(:,i)+Ts*imdyn(1,exkk(:,i),u);
+    ePkk1(:,:,i)=phi*ePkk(:,:,i)*phi' +gamad*Q*gamad';
+    % Kalman gain computation
+    Lk=ePkk1(:,:,i)*C'*inv(C*ePkk1(:,:,i)*C'+R);
+    % Update step
+    e(:,i)=ey(:,i)-C*exkk1(:,i);
+    exkk(:,i+1)=exkk1(:,i) + Lk*e(:,i);
+    ePkk(:,:,i+1)=(eye(5)-Lk*C)*ePkk1(:,:,i);
+end
+%% plots for EKF
+figure(2)
+subplot(321),plot(T,x(1,:),T,exkk(1,:)), ylabel('x_1 (flux)'), title('State Variables'),legend('true', 'estimated')
+subplot(322),plot(T,x(2,:),T,exkk(2,:)), ylabel('x_2 (flux)'), title('State Variables'),legend('true', 'estimated')
+subplot(323),plot(T,x(3,:),T,exkk(3,:)), ylabel('x_3 (flux)'), title('State Variables'),legend('true', 'estimated')
+subplot(324),plot(T,x(4,:),T,exkk(4,:)), ylabel('x_4 (flux)'), title('State Variables'),legend('true', 'estimated')
+subplot(325),plot(T,x(5,:),T,exkk(5,:)), ylabel('x_5 (angular velocity)'), title('State Variables'),legend('true', 'estimated')
+    
+    
 
     
     
